0e018261bd154f817f48b8e6ad04cd34942e8553
vishy08 <vishyk@live.unc.edu>
2022-08-23 22:07:25 -0400

    fitbit api data

--- myapp.py
+++ myapp.py
@@ -1,4 +1,5 @@
 import requests, json
-resp = requests.get("https://v2.jokeapi.dev/joke/Programming?type=single")
-x = json.loads(resp.text)
-print(x["joke"])
+myheader = {"Authorization" : "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzhRUTkiLCJzdWIiOiJCNEYzNVEiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJzZXQgcm94eSBycHJvIHJudXQgcnNsZSByYWN0IHJyZXMgcmxvYyByd2VpIHJociBydGVtIiwiZXhwIjoxNjYxMzUwODMzLCJpYXQiOjE2NjA3NDYwMzN9.-6KKpR38nXavOTwsBrJBKuMQD3thwGeFDguLTMiOPM0"}
+myurl = "https://api.fitbit.com/1/user/-/activities/steps/date/2019-01-01/7d.json"
+resp = requests.get(myurl, headers=myheader).json()
+print(resp)
